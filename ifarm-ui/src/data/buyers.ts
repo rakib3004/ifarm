32052decb2a447a6d437799a5a95a762c075790a
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-11-08 22:41:34 +0600

    add data in buyers and sellers

---
+++ ifarm-ui/src/data/buyers.ts
@@ -0,0 +1,207 @@
+export const buyers = [
+    {
+        "id": "a4e7b8c5-1d3a-4f5c-b7e9-2c8a9d7f4b5d",
+        "name": "City Grocery Center",
+        "phone_number": "+8801734567890",
+        "address": {
+            "type": "urban",
+            "house": "34/A",
+            "road": "10",
+            "place": "Banani",
+            "p.o": "Banani",
+            "thana": "Banani",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d9c5a2f8-4e3b-4f7d-b6c1-3e4d9f0b8c6a",
+        "name": "Dhaka General Store",
+        "phone_number": "+8801856789123",
+        "address": {
+            "type": "urban",
+            "house": "45/B",
+            "road": "5",
+            "place": "Dhanmondi",
+            "p.o": "Dhanmondi",
+            "thana": "Dhanmondi",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "b3d8e9a2-5f4c-4b7e-b1c5-2e9f3d8c7b5a",
+        "name": "Uttara Super Shop",
+        "phone_number": "+8801987654321",
+        "address": {
+            "type": "urban",
+            "house": "12/C",
+            "road": "14",
+            "place": "Uttara",
+            "p.o": "Uttara",
+            "thana": "Uttara",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "c7b9e2a3-1f5d-4c8f-b9d0-2e4d8a3f6b7d",
+        "name": "Rupnogor Bazar",
+        "phone_number": "+8801934567890",
+        "address": {
+            "type": "urban",
+            "house": "85/B",
+            "road": "22",
+            "place": "Mohammadpur",
+            "p.o": "Mohammadpur",
+            "thana": "Mohammadpur",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d5f6e3b9-4c8d-4f9a-b7e2-5c3a8b2f0d7b",
+        "name": "Mirpur General Store",
+        "phone_number": "+8801678123456",
+        "address": {
+            "type": "urban",
+            "house": "23/A",
+            "road": "4",
+            "place": "Mirpur",
+            "p.o": "Mirpur",
+            "thana": "Mirpur",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "e3c9f5a4-6b7e-4a6f-b2d3-9f1c8e7d4b0a",
+        "name": "Deshi Groceries",
+        "phone_number": "+8801789456123",
+        "address": {
+            "type": "rural",
+            "village": "Chandipur",
+            "post_office": "Sonagazi",
+            "thana": "Sonagazi",
+            "zilla": "Feni"
+        }
+    },
+    {
+        "id": "c9b2f7e6-3f5c-4b8d-a6e2-5b4a7f0c2d9f",
+        "name": "Green Mart",
+        "phone_number": "+8801578901234",
+        "address": {
+            "type": "urban",
+            "house": "56/A",
+            "road": "3",
+            "place": "Gulshan",
+            "p.o": "Gulshan",
+            "thana": "Gulshan",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d7a5e3c9-5f7b-4b8f-a1c2-8b4d2f6a0c7e",
+        "name": "Kishan Bazar",
+        "phone_number": "+8801867456789",
+        "address": {
+            "type": "urban",
+            "house": "70/B",
+            "road": "15",
+            "place": "Shyamoli",
+            "p.o": "Shyamoli",
+            "thana": "Shyamoli",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "a4c6b2e8-5f3c-4d8b-a1e9-2f5d4a0c7b9f",
+        "name": "Krishi Mart",
+        "phone_number": "+8801887654321",
+        "address": {
+            "type": "urban",
+            "house": "78/C",
+            "road": "25",
+            "place": "Nikunja",
+            "p.o": "Nikunja",
+            "thana": "Khilkhet",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d6f5a7b9-3c4e-4a8d-b7c2-3e9f4d8b2c7f",
+        "name": "Nababpur Bazar",
+        "phone_number": "+8801789123456",
+        "address": {
+            "type": "urban",
+            "house": "44/B",
+            "road": "8",
+            "place": "Mugda",
+            "p.o": "Mugda",
+            "thana": "Mugda",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "b2e7f3a4-5d8c-4b9a-b6e1-4c7f2a3d9b0f",
+        "name": "Mukti Grocery Store",
+        "phone_number": "+8801598345678",
+        "address": {
+            "type": "urban",
+            "house": "15/A",
+            "road": "19",
+            "place": "Khilgaon",
+            "p.o": "Khilgaon",
+            "thana": "Khilgaon",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "c8b2f5d4-1e7f-4c9b-a5e3-2d9f4b0c6a7e",
+        "name": "Jannat Grocery Shop",
+        "phone_number": "+8801567891234",
+        "address": {
+            "type": "urban",
+            "house": "65/C",
+            "road": "6",
+            "place": "Adabor",
+            "p.o": "Adabor",
+            "thana": "Adabor",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "a7f3b2e9-6c4a-4b8f-a7c1-5f3d9b0e2a7f",
+        "name": "Khilgaon General Store",
+        "phone_number": "+8801789345678",
+        "address": {
+            "type": "urban",
+            "house": "31/A",
+            "road": "12",
+            "place": "Khilgaon",
+            "p.o": "Khilgaon",
+            "thana": "Khilgaon",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d8b5c2a9-7f4c-4e3a-b7d2-5c4f3e0b6d9a",
+        "name": "Bangla Bazar Store",
+        "phone_number": "+8801967456789",
+        "address": {
+            "type": "rural",
+            "village": "Churamonkathi",
+            "post_office": "Churamonkathi",
+            "thana": "Jessore Sadar",
+            "zilla": "Jessore"
+        }
+    },
+    {
+        "id": "e4b8c3a9-6f7b-4d9c-a7e2-3f5a8d0c6b9e",
+        "name": "Shahbagh Grocery",
+        "phone_number": "+8801634567890",
+        "address": {
+            "type": "urban",
+            "house": "22/B",
+            "road": "5",
+            "place": "Shahbagh",
+            "p.o": "Shahbagh",
+            "thana": "Shahbagh",
+            "zilla": "Dhaka"
+        }
+    } ]
---
+++ ifarm-ui/src/data/sellers.ts
@@ -0,0 +1,192 @@
+export const sellers = [
+    {
+        "id": "3c62b9ad-e7f4-4d5d-b0d4-30ef2f03ae47",
+        "name": "Grameen Agro Farm",
+        "phone_number": "+8801734567890",
+        "transaction_type": "bkash",
+        "address": {
+            "type": "rural",
+            "village": "Chandipur",
+            "post_office": "Sonagazi",
+            "thana": "Sonagazi",
+            "zilla": "Feni"
+        }
+    },
+    {
+        "id": "a732b44c-61e9-4cb1-92b9-458f4a85d6ad",
+        "name": "Shobuj Krishi",
+        "phone_number": "+8801851234567",
+        "transaction_type": "nagad",
+        "address": {
+            "type": "rural",
+            "village": "Kharicha",
+            "post_office": "Boda",
+            "thana": "Boda",
+            "zilla": "Panchagarh"
+        }
+    },
+    {
+        "id": "d5f62ef0-8a8c-4e3a-8a77-6f4b85d0f2de",
+        "name": "Palli Krishi Bazar",
+        "phone_number": "+8801632135467",
+        "transaction_type": "upay",
+        "address": {
+            "type": "rural",
+            "village": "Kanupur",
+            "post_office": "Kaharole",
+            "thana": "Kaharole",
+            "zilla": "Dinajpur"
+        }
+    },
+    {
+        "id": "a8c4e2a9-9f6b-4b2b-a3cb-b6a9c7d9f345",
+        "name": "Jibon Krishi Kendra",
+        "phone_number": "+8801987654321",
+        "transaction_type": "rocket",
+        "address": {
+            "type": "rural",
+            "village": "Mohadevpur",
+            "post_office": "Mohadevpur",
+            "thana": "Mohadevpur",
+            "zilla": "Naogaon"
+        }
+    },
+    {
+        "id": "b3e7f98c-2c5b-4a6b-bc39-95b3f5d4e3b7",
+        "name": "Muktir Agro House",
+        "phone_number": "+8801798765432",
+        "transaction_type": "bkash",
+        "address": {
+            "type": "urban",
+            "house": "45/B",
+            "road": "23",
+            "place": "Mirpur",
+            "p.o": "Mirpur",
+            "thana": "Mirpur",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "f0b4a3e8-0d6f-4f3d-b6c9-2c9f8d7a4c76",
+        "name": "Desh Krishi Store",
+        "phone_number": "+8801787654321",
+        "transaction_type": "nagad",
+        "address": {
+            "type": "urban",
+            "house": "30/C",
+            "road": "10",
+            "place": "Uttara",
+            "p.o": "Uttara",
+            "thana": "Uttara",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "e9a5d6c7-5f3a-4b8b-a6c2-4d6b7f2a9d8e",
+        "name": "Alo Krishi Vanijya",
+        "phone_number": "+8801589765432",
+        "transaction_type": "upay",
+        "address": {
+            "type": "rural",
+            "village": "Bokchora",
+            "post_office": "Birganj",
+            "thana": "Birganj",
+            "zilla": "Dinajpur"
+        }
+    },
+    {
+        "id": "c5d9f2a7-4e3b-4b7c-b3c2-2e7d4a9b0c76",
+        "name": "Kishan Krishi Bikash",
+        "phone_number": "+8801934567890",
+        "transaction_type": "rocket",
+        "address": {
+            "type": "rural",
+            "village": "Kashipur",
+            "post_office": "Shahjadpur",
+            "thana": "Shahjadpur",
+            "zilla": "Sirajganj"
+        }
+    },
+    {
+        "id": "b7a5f3d9-1c4e-4b8d-b6c2-9f3d8a2c7b5f",
+        "name": "Probhat Agro Center",
+        "phone_number": "+8801765432109",
+        "transaction_type": "bkash",
+        "address": {
+            "type": "urban",
+            "house": "120/A",
+            "road": "5",
+            "place": "Banani",
+            "p.o": "Banani",
+            "thana": "Banani",
+            "zilla": "Dhaka"
+        }
+    },
+    {
+        "id": "d6a4b3e7-2d5c-4f7b-b9c1-3e9a5c0b7f6a",
+        "name": "Deshbondu Agro Farm",
+        "phone_number": "+8801532145678",
+        "transaction_type": "nagad",
+        "address": {
+            "type": "rural",
+            "village": "Kazipur",
+            "post_office": "Kazipur",
+            "thana": "Kazipur",
+            "zilla": "Sirajganj"
+        }
+    },
+    {
+        "id": "f5d4b2a6-7f9b-4a6d-b3c7-4d2a8b0c6e9f",
+        "name": "Chaya Agro",
+        "phone_number": "+8801932145678",
+        "transaction_type": "upay",
+        "address": {
+            "type": "rural",
+            "village": "Narayanganj",
+            "post_office": "Narayanganj",
+            "thana": "Narayanganj Sadar",
+            "zilla": "Narayanganj"
+        }
+    },
+    {
+        "id": "c4e7d9a2-5f3b-4b8d-a2b1-9e3a7f5b6c0d",
+        "name": "Bangla Agro Bhandar",
+        "phone_number": "+8801823456789",
+        "transaction_type": "rocket",
+        "address": {
+            "type": "rural",
+            "village": "Sathia",
+            "post_office": "Sathia",
+            "thana": "Sathia",
+            "zilla": "Pabna"
+        }
+    },
+    {
+        "id": "b8a5d4f7-2e9c-4b8b-b6c1-2e3d4f6b9c7a",
+        "name": "Mati Krishi",
+        "phone_number": "+8801987654320",
+        "transaction_type": "nagad",
+        "address": {
+            "type": "rural",
+            "village": "Shimulbari",
+            "post_office": "Shimulbari",
+            "thana": "Boda",
+            "zilla": "Panchagarh"
+        }
+    },
+    {
+        "id": "c7e9d2b4-1f3a-4c8d-b5a1-4d6a8f0c7e5d",
+        "name": "Sonar Bangla Krishi",
+        "phone_number": "+8801589765430",
+        "transaction_type": "bkash",
+        "address": {
+            "type": "urban",
+            "house": "32/B",
+            "road": "7",
+            "place": "Dhanmondi",
+            "p.o": "Dhanmondi",
+            "thana": "Dhanmondi",
+            "zilla": "Dhaka"
+        }
+    }
+]
