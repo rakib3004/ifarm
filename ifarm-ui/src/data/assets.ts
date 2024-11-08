e8bf57d3ea0b6508a04e655b260f636a5e6d010d
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2024-11-08 22:13:51 +0600

    add asset data

---
+++ ifarm-ui/src/data/assets.ts
@@ -0,0 +1,56 @@
+export const assets = [
+    {
+        "id": "a1e74ad4-7b6d-4d3d-90e5-0f6bb9f1a112",
+        "name": "egg",
+        "image": {
+            "id": "b3f9a9c5-2d5c-4383-85c7-a3e9209c7dfb"
+        },
+        "price": 8,
+        "price_unit": "per piece"
+    },
+    {
+        "id": "c9c7e6b4-1f6b-4b72-bf2a-124fd6a3d2c3",
+        "name": "potato",
+        "image": {
+            "id": "d6b5f4a7-9f1b-493b-b10a-e12f2c6c8b7a"
+        },
+        "price": 60,
+        "price_unit": "per kg"
+    },
+    {
+        "id": "f0a1e1c6-5c2b-4a8c-93cf-5bb0c2f0aef3",
+        "name": "onion",
+        "image": {
+            "id": "c4b8d9e0-6a2f-4b7b-a1c5-e34f9a1d5f2e"
+        },
+        "price": 130,
+        "price_unit": "per kg"
+    },
+    {
+        "id": "a9d2b0c7-3f7a-4930-86d1-0f3e7f5b4a8c",
+        "name": "rice",
+        "image": {
+            "id": "e3c8f6a9-2d8b-479f-ae2b-dc9a7f0e6f8b"
+        },
+        "price": 55,
+        "price_unit": "per kg"
+    },
+    {
+        "id": "b1f8c2a7-9a6e-46f2-8579-5f3d7e0c9f7a",
+        "name": "garlic",
+        "image": {
+            "id": "f9e0c4a1-6d2a-4b6c-b9c1-df8e7a1f2b6c"
+        },
+        "price": 110,
+        "price_unit": "per kg"
+    },
+    {
+        "id": "d4a9e6b3-1c2a-4f9d-b8c1-7f0d6a2b3c9e",
+        "name": "dal",
+        "image": {
+            "id": "c3f7a8e1-5d2b-4b7e-bc1a-9f2d6a0c3b1e"
+        },
+        "price": 115,
+        "price_unit": "per kg"
+    }
+]
