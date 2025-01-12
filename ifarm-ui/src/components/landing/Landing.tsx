c4125318225a746dc1dd7c79871e9b83b07ba2ee
Md. Rakib Trofder <bsse1129@iit.du.ac.bd>
2025-01-12 23:11:26 +0600

    add landing page

--- ifarm-ui/src/components/landing/Landing.tsx
+++ ifarm-ui/src/components/landing/Landing.tsx
@@ -1,10 +1,4 @@
-
-import {
-  Sidebar,
-  SidebarBody,
-  SidebarItem,
-  SidebarList
-} from 'keep-react';
+import { Sidebar, SidebarBody, SidebarItem, SidebarList } from "keep-react";
 import {
   BoundingBox,
   Heart,
@@ -13,58 +7,61 @@ import {
   PersonSimple,
   ShoppingBag,
   Truck,
-  User
-} from 'phosphor-react';
+  User,
+} from "phosphor-react";
 const Landing = () => {
-    return (
-        <Sidebar>
-          <SidebarBody>
-            <SidebarList>
-              <SidebarItem>
-                <HouseLine size={20} />
-                Home
-              </SidebarItem>
-              <SidebarItem>
-                <BoundingBox size={20} />
-                Products
-              </SidebarItem>
-              <SidebarItem>
-                <MapPinLine size={20} />
-                Track
-              </SidebarItem>
-              <SidebarItem>
-                <PersonSimple size={20} />
-                Sellers
-              </SidebarItem>
-              <SidebarItem>
-                <User size={20} />
-                Buyers
-              </SidebarItem>
-              <SidebarItem>
-                <Truck size={20} />
-                Transport
-              </SidebarItem>
-              <SidebarItem>
-                <ShoppingBag size={20} />
-                Trade
-              </SidebarItem>
-              <SidebarItem>
-                <Heart size={20} />
-                Support
-              </SidebarItem>
-            </SidebarList>
-          </SidebarBody>
-          {/* <SidebarFooter>
-            <Avatar>
-              <AvatarImage src="/images/avatar/avatar-1.png" alt="avatar" />
-              <AvatarFallback>KR</AvatarFallback>
-            </Avatar>
-            <div>
-               <p className="text-body-4 font-medium text-metal-400 dark:text-white">Enzo Farnandez</p>
-               <p className="text-body-4 font-normal text-metal-300 dark:text-metal-400">enzo123@gmail.com</p>
-            </div>
-          </SidebarFooter> */}
-        </Sidebar>
-      )
-}
+  return (
+<div className="flex">
+  <div>
+  <Sidebar>
+      <SidebarBody>
+        <img
+          src="images/logo.png"
+          alt="speaker"
+          className="rounded-full w-16 h-16"
+        />{" "}
+        <SidebarList>
+          <SidebarItem>
+            <HouseLine size={20} />
+            Home
+          </SidebarItem>
+          <SidebarItem>
+            <BoundingBox size={20} />
+            Products
+          </SidebarItem>
+          <SidebarItem>
+            <MapPinLine size={20} />
+            Track
+          </SidebarItem>
+          <SidebarItem>
+            <PersonSimple size={20} />
+            Sellers
+          </SidebarItem>
+          <SidebarItem>
+            <User size={20} />
+            Buyers
+          </SidebarItem>
+          <SidebarItem>
+            <Truck size={20} />
+            Transport
+          </SidebarItem>
+          <SidebarItem>
+            <ShoppingBag size={20} />
+            Trade
+          </SidebarItem>
+          <SidebarItem>
+            <Heart size={20} />
+            Support
+          </SidebarItem>
+        </SidebarList>
+      </SidebarBody>
+    </Sidebar>
+  </div>
+  <div>
+    <h1>Welcome to Crop Chain</h1>
+<img src="images/landing.png" alt="Crop Chain" />
+  </div>
+</div>
+  );
+};
 export default Landing;
